af8fbd85db1c9ef62f056944a7e5efecdc31d917
Courtney Adams <cadams8238@gmail.com>
2018-11-02 10:31:07 -0700

    added content to LandingPage component, unstyled

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const LandingPage = () => {
-	return <div>hello world</div>;
+	return (
+		<div>
+			<header>
+				<h1>
+					PDX <span>Rides</span>
+				</h1>
+			</header>
+			<p>
+				Portland, OR is known to be one of the best cities in the US for
+				bicyclists. Scroll to learn more about Portland bike culture and see why
+				Portlanders love their two-wheeled friends.
+			</p>
+		</div>
+	);
 };
 
 export default LandingPage;
