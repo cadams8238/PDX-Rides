71aa997a77c170d5d66d492d6f75594279e17baa
Courtney Adams <cadams8238@gmail.com>
2018-11-03 18:32:33 -0700

    added basic grid, needs centering/positioning fine tuning

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,17 +4,17 @@ import styles from "./styles/LandingPage.module.css";
 const LandingPage = () => {
 	return (
 		<div className={styles.background}>
-			<header>
+			<header className={styles.header}>
 				<img
 					className={styles.bikeIcon}
 					src={require("../assets/bike.svg")}
 					alt="bike icon"
 				/>
-				<h1>
-					PDX <span>Rides</span>
+				<h1 className={styles.h1_PDX}>
+					PDX <span className={styles.h1_Rides}>Rides</span>
 				</h1>
 			</header>
-			<p>
+			<p className={styles.introDescription}>
 				Portland, OR is known to be one of the best cities in the US for
 				bicyclists. Scroll to learn more about Portland bike culture and see why
 				Portlanders love their two-wheeled friends.
