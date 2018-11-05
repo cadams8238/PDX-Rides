4c15203582e4f5d436c06240757f6a5841cd2e0f
Courtney Adams <cadams8238@gmail.com>
2018-11-04 16:03:40 -0800

    added first part of Overview component content including city name and ranking

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./styles/Overview.module.css";
 
 const Overview = () => {
-	return <div className={styles.hello}>hello world</div>;
+	return (
+		<section>
+			<div>
+				<h2>Portland, OR</h2>
+				<p>
+					5<span>th</span>
+				</p>
+				<p>Best City for Bikes</p>
+			</div>
+		</section>
+	);
 };
 
 export default Overview;
